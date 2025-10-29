8403e8e96a674af8f88b58c17ff7a55c8dbaa7d0
Isabelle Nguyen <isabellenguyen@Isabelles-MacBook-Pro-8.local>
2025-10-29 17:08:13 -0500

    Initial commit: Hello World app

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Define a route
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/user', (req, res) => {
+    res.send('Hi User!');
+});
+
+// Start the server
+app.listen(port, () => {
+console.log(`Example app listening at http://localhost:${port}`);
+});
